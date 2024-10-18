0ecdb1c832a28970e47d14911044051463acc78c
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-10-18 12:40:57 +0700

    vercel 9

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,24 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Handler } from 'aws-lambda';
+import { Handler, Context, APIGatewayEvent } from 'aws-lambda';
 import serverlessExpress from '@vendia/serverless-express';
 import './cloudinary.config'; // Import the Cloudinary configuration
 
-async function bootstrap(): Promise<Handler> {
+let cachedServer: Handler;
+
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+  cachedServer = serverlessExpress({ app: expressApp });
 }
 
-let cachedServer: Handler;
-
-export const handler: Handler = async (event, context) => {
+export const handler: Handler = async (event: APIGatewayEvent, context: Context) => {
   if (!cachedServer) {
-    cachedServer = await bootstrap();
+    await bootstrap(); // Initialize the server if it hasn't been cached
   }
   return cachedServer(event, context, () => { });
 };
