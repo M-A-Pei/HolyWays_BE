5ff1c63316e57eccc7b50005eab3b9bc0e64d233
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-10-24 21:03:17 +0700

    logged all other variables

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import './cloudinary.config'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log("database url: ", process.env.DATABASE_URL)
+  console.log("direct url: ", process.env.DIRECT_URL)
+  console.log("cloudinary name: ", process.env.CLOUDINARY_CLOUD_NAME)
   console.log("node env: ", process.env.NODE_ENV)
   app.enableCors();
   await app.listen(3000);
